9edaf9a1f091a3237a604311302852899250683c
pradyX <pradyumnbhushan@gmail.com>
2021-08-27 22:20:57 +0530

    make it smol

--- counter.sol
+++ counter.sol
@@ -1,17 +1,9 @@
 pragma solidity ^0.8.4;
 
 contract Counter{
-    uint count;
-    
-    constructor () public {
-        count = 0;
-    }
-    
-    function getCount() public view returns(uint) {
-        return count;
-    }
+    uint public count=1;
     
     function incrementCount() public {
-        count += 1;
+        count ++;
     }
 }
